5128b8ae83f52b61996dadb0993913cd6bafcdf4
Nir Hemo <nirhemo@gmail.com>
2026-02-10 21:05:07 +0200

    Fix: remove position number from exit popup

--- src/components/ExitIntentPopup.tsx
+++ src/components/ExitIntentPopup.tsx
@@ -155,7 +155,7 @@ export default function ExitIntentPopup() {
                 </div>
                 <h3 className="text-2xl font-bold mb-2">You&apos;re in!</h3>
                 <p className="text-secondary font-semibold">
-                  You&apos;re #{result.position} on the waitlist
+                  Thank you! We&apos;ll be in touch soon
                 </p>
               </motion.div>
             )}
